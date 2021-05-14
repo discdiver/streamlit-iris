5f5ca83a25ca8740b0431d6b0eb80640d94cf0a3
discdiver <jeffmshale@gmail.com>
2021-05-14 13:42:12 -0400

    remove color picker

--- app.py
+++ app.py
@@ -6,14 +6,11 @@ from load_data import load_iris as li
 import time
 
 
- 
 # with st.spinner(text='In progress'):
 #     time.sleep(5)
 #     st.success('Done')
 
-file = st.file_uploader('File uploader')
-
-st.color_picker('pick a color')
+file = st.file_uploader("File uploader")
 
 st.title("My Awesome Flower Predictor")
 st.header("We predict Iris types")
@@ -23,26 +20,26 @@ st.subheader("No joke")
 df_iris = li()
 
 
-st.plotly_chart(px.scatter(df_iris, 'sepal_width', 'sepal_length'))
+st.plotly_chart(px.scatter(df_iris, "sepal_width", "sepal_length"))
 
 
 show_df = st.checkbox("Do you want to see the data?")
 
-if show_df: 
+if show_df:
     df_iris
 
 # get user flower input
 
-s_l = st.number_input('Input the Sepal Length')
-s_w = st.number_input('Input the Sepal Width')
-p_l = st.number_input('Input the Petal Length')
-p_w = st.number_input('Input the Petal Width')
+s_l = st.number_input("Input the Sepal Length")
+s_w = st.number_input("Input the Sepal Width")
+p_l = st.number_input("Input the Petal Length")
+p_w = st.number_input("Input the Petal Width")
 
 user_values = np.array([s_l, s_w, p_l, p_w])
 
 # load model
 
-with open('saved-iris-model-2.pkl', 'rb') as f:
+with open("saved-iris-model-2.pkl", "rb") as f:
     model = pickle.load(f)
 
 with st.echo():
@@ -57,15 +54,14 @@ with st.echo():
 
 # st.write(type(prediction))
 
-st.header(f'The model predicts: {prediction[0]}')
+st.header(f"The model predicts: {prediction[0]}")
 
 # st.balloons()
 
 
-
 col1, col2, col3 = st.beta_columns(3)
 with col1:
-    'I am printing things'
+    "I am printing things"
 with col2:
     df_iris
 with col3:
